1291c147a4c6f34b6b0f85a76ef55db89718e5e4
chacalonchacaloso <pcorqui@gmail.com>
2020-10-02 00:44:35 -0500

    rutas, controladores y vistas agregadas

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+//Paso numero 1 se hace la ruta **********************
+//Paso numero 2 se va al controlador(en el primer caso PageController)
 //Ruta raiz cuando se entra a la paguina mostrara la vista welcome
-Route::get('/', function () {
+
+Route::get('/',[App\Http\Controllers\PageController::class,'posts']);
+Route::get('blog/{post}',[App\Http\Controllers\PageController::class,'post'])->name('post');
+
+//ruta por default
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
 
 Auth::routes();
 
